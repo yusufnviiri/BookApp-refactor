ac94f4dabc27ef9bde29f7dcd77b3ebe4dd03e53
yusufnviiri <yusufnviiri@gmail.com>
2022-04-21 02:29:31 +0300

    Modify script.js

--- script.js
+++ script.js
@@ -56,15 +56,40 @@ function removeBook(i) {
   updateLocalStorage();
   showBooks();
 }
-
+const bookItems = document.querySelector('#book-list');
+const sectionTitle = document.querySelector('#section-title');
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
-
   addBook(title.value, author.value);
+  sectionTitle.style.display = 'block';
+  bookItems.style.display = 'flex';
 });
 
 window.onload = showBooks();
+
+const displayBooks = document.querySelector('#display-list');
+const displayForm = document.querySelector('#display-form');
+const contact = document.querySelector('#contact');
+const container = document.querySelector('.container');
+const addBookList = document.querySelector('#form');
+const formSubmit = document.querySelector('#add-book');
+
+function displayFormonly() {
+  container.style.backgroundColor = '#ffcf33';
+  addBookList.style.display = 'flex';
+  sectionTitle.style.display = 'none';
+  bookItems.style.display = 'none';
+  contact.style.display = 'none';
+}
+function displayBookListonly() {
+  container.style.backgroundColor = '#ffa833';
+  addBookList.style.display = 'none';
+  contact.style.display = 'none';
+}
+displayBooks.addEventListener('click', displayBookListonly);
+
+displayForm.addEventListener('click', displayFormonly);
