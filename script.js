26fb551e9f83a784ddb6783eb53a12e312f4298c
yusufnviiri <yusufnviiri@gmail.com>
2022-04-21 09:37:18 +0300

    Add contact section

--- script.js
+++ script.js
@@ -74,9 +74,11 @@ window.onload = showBooks();
 const displayBooks = document.querySelector('#display-list');
 const displayForm = document.querySelector('#display-form');
 const contact = document.querySelector('#contact');
+const displayContact = document.querySelector('#display-contact');
 const container = document.querySelector('.container');
 const addBookList = document.querySelector('#form');
 const formSubmit = document.querySelector('#add-book');
+const separator = document.querySelector('.separator');
 
 function displayFormonly() {
   container.style.backgroundColor = '#ffcf33';
@@ -84,12 +86,38 @@ function displayFormonly() {
   sectionTitle.style.display = 'none';
   bookItems.style.display = 'none';
   contact.style.display = 'none';
+  separator.style.display = 'none';
 }
 function displayBookListonly() {
   container.style.backgroundColor = '#ffa833';
   addBookList.style.display = 'none';
   contact.style.display = 'none';
+  sectionTitle.style.display = 'block';
+  bookItems.style.display = 'flex';
 }
+function displayContactonly() {
+  container.style.backgroundColor = '#ffa833';
+  addBookList.style.display = 'none';
+  sectionTitle.style.display = 'none';
+  bookItems.style.display = 'none';
+  contact.style.display = 'flex';
+  separator.style.display = 'none';
+}
+
+contact.innerHTML = `<h2>
+Contact information
+</h2>
+<p> Do you have any questions or you just want to say "Hello"
+  <br>You can reach out to us!
+  </p>
+<ul>
+  <li>Our email:mail@mail.com</li>
+  <li>Our Phone number:004386534422</li>
+  <li>Our address:Streetname 22,88273 Islamabad,Pakistan </li>
+</ul>`;
+
 displayBooks.addEventListener('click', displayBookListonly);
 
 displayForm.addEventListener('click', displayFormonly);
+
+displayContact.addEventListener('click', displayContactonly);
